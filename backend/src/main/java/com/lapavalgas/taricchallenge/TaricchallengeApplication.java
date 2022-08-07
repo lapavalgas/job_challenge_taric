8b7d3cbc263bbb466c0932c7383b293fc6183d32
lapavalgas <lapavalgas@gmail.com>
2022-08-07 02:08:36 -0300

    open cors

--- backend/src/main/java/com/lapavalgas/taricchallenge/TaricchallengeApplication.java
+++ backend/src/main/java/com/lapavalgas/taricchallenge/TaricchallengeApplication.java
@@ -7,12 +7,29 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TaricchallengeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaricchallengeApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(final CorsRegistry registry) {
+                registry.addMapping("/graphql/**")
+                        .allowedOrigins(CorsConfiguration.ALL)
+                        .allowedHeaders(CorsConfiguration.ALL)
+                        .allowedMethods(CorsConfiguration.ALL);
+            }
+        };
     }
 
     @Bean
--- backend/src/main/java/com/lapavalgas/taricchallenge/domain/service/LoginService.java
+++ backend/src/main/java/com/lapavalgas/taricchallenge/domain/service/LoginService.java
@@ -2,12 +2,18 @@ package com.lapavalgas.taricchallenge.domain.service;
 
 import com.lapavalgas.taricchallenge.domain.DTO;
 import com.lapavalgas.taricchallenge.domain.MSG;
+import org.springframework.context.annotation.Bean;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Controller
 public class LoginService {
+
     @SchemaMapping(typeName = "Mutation", value = "login")
     public static DTO login(@Argument String user, @Argument String pass) {
         var dto = new DTO();
@@ -29,4 +35,5 @@ public class LoginService {
         var dto = new DTO();
         return dto.setHttpResponse(MSG.STATUS_CODE_200, MSG.USER_LOGOFF);
     }
+
 }
