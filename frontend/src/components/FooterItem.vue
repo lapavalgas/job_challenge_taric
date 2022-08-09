dbb360d42c4b9ba320b38108cbbc6d620ad7b1ae
lapavalgas <lapavalgas@gmail.com>
2022-08-09 15:44:56 -0300

    footer refinamento

--- frontend/src/components/FooterItem.vue
+++ frontend/src/components/FooterItem.vue
@@ -1,7 +1,23 @@
 <template>
     <div id="me">® Developed by
-        <a href="https://www.linkedin.com/in/lapavalgas/"><span>Rafael Lapa Valgas</span></a>
-        as a technical challenge for a job interview at <span class="taric">TARIC startup</span> from
-        <span class="zitrus">Zitrus Healthtech</span>.
+        <a href="https://www.linkedin.com/in/lapavalgas/"><span class="myname">Rafael Lapa Valgas</span></a> as a
+        technical challenge for a job interview at
+        <a href="https://zitrus.com.br/taric/"><span class="taric">TARIC startup</span></a>from
+        <a href="https://zitrus.com.br/"><span class="zitrus">Zitrus Healthtech</span></a>.
     </div>
 </template>
+<style>
+.myname {
+    color: black;
+}
+
+.taric {
+    color: black;
+    background-color: var(--clr-quaternary);
+}
+
+.zitrus {
+    color: black;
+    background-color: var(--clr-secondary);
+}
+</style>
